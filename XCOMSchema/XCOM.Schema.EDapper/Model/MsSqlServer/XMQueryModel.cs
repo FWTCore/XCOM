dbf2db314a17202e197ae9c85617f6612399b176
xcom <121086507@qq.com>
2022-04-17 22:29:02 +0800

    修改mssql语句解析

--- XCOMSchema/XCOM.Schema.EDapper/Model/MsSqlServer/XMQueryModel.cs
+++ XCOMSchema/XCOM.Schema.EDapper/Model/MsSqlServer/XMQueryModel.cs
@@ -14,65 +14,6 @@ namespace XCOM.Schema.EDapper.Model.MsSqlServer
         }
 
 
-        public override string SelectSql
-        {
-            get
-            {
-                return $"select {this.FieldsSql} from {GetTableName()} {this.WhereSql} {this.OrderBySql} {this.TopSql}";
-            }
-        }
-
-        public override string SqlFetchSql_2005_2008
-        {
-            get
-            {
-                if (SkipCount == null || SkipCount == 0)
-                {
-                    return $"select {this.FieldsSql} from {GetTableName()} {this.WhereSql} {this.OrderBySql} {this.TopSql}";
-                }
-                else
-                {
-                    return $"select {this.FieldsSql} from {GetTableName()} {this.WhereSql} {this.OrderBySql} LIMIT {SkipCount},{FetchCount}";
-                }
-            }
-        }
-
-        public override string SqlFetchSql
-        {
-            get
-            {
-                if (SkipCount == null || SkipCount == 0)
-                {
-                    return $"select {this.FieldsSql} from {GetTableName()} {this.WhereSql} {this.OrderBySql} {this.TopSql}";
-                }
-                else
-                {
-                    return $"select {this.FieldsSql} from {GetTableName()} {this.WhereSql} {this.OrderBySql} LIMIT {SkipCount},{FetchCount}";
-                }
-            }
-        }
-
-        public override string CountSql
-        {
-            get
-            {
-                return $"select Count(1) from {GetTableName()} {this.WhereSql}";
-            }
-        }
-
-        internal override string TopSql
-        {
-            get
-            {
-                if (this.FetchCount != null && this.FetchCount > 0)
-                {
-                    return $"LIMIT {this.FetchCount}";
-                }
-                return "";
-            }
-        }
-
-
 
     }
 }
