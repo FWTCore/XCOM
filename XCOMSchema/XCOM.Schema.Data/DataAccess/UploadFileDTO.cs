d48c42bf2d8d974816a7971a7c190d40e6c4fcdd
xuchase <121086507@qq.com>
2022-10-16 18:46:52 +0800

    添加注释

--- XCOMSchema/XCOM.Schema.Data/DataAccess/UploadFileDTO.cs
+++ XCOMSchema/XCOM.Schema.Data/DataAccess/UploadFileDTO.cs
@@ -13,9 +13,21 @@ namespace XCOM.Schema.Data.DataAccess
         {
             ContentType = "application/octet-stream";
         }
+        /// <summary>
+        /// 名称
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// 文件名称
+        /// </summary>
         public string Filename { get; set; }
+        /// <summary>
+        /// 文件内容类型
+        /// </summary>
         public string ContentType { get; set; }
+        /// <summary>
+        /// 数据流
+        /// </summary>
         public Stream Stream { get; set; }
 
     }
